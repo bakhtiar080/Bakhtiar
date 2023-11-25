72bbac27fdbab045dc975e9f8fa686de3fe3455d
Bakhtiar13 <bakhtiar080@gmail.com>
2023-11-25 19:35:13 +0700

    adding-files

---
+++ resources/js/views/addon/Audio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <audio id="audio" muted autoplay loop playsinline>
+        <source src="assets/music/Bersamamu.mp3" type="audio/mpeg">
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
